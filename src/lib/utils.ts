653935a364cef7eafe6c9f65b9f652273d998296
Vinicius Roosevelt <catce.2021111tads@aluno.ifpi.edu.br>
2025-07-31 21:48:42 -0300

    feat: add news section in main page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function scrollToId(id: string) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({
+      behavior: 'smooth', // animação suave
+      block: 'start',     // alinha ao topo do viewport
+    });
+  }
+}
